8cd45570794765e79744e4e9b69fc468d56a2597
Kamila Antonina <kamilaat99@gmail.com>
2018-09-04 17:00:15 +0000

    Algorithm written for sequence

--- max_int.py
+++ max_int.py
@@ -1,15 +1,17 @@
-#Design an algorithm that finds the maximum positive integer input by a user. 
-#The user repeatedly inputs numbers until a negative value is entered. 
+n = int(input("Enter the length of the sequence: "))
 
-num_int = int(input("Input a number: "))    # Do not change this line
-max_int = num_int
+num1 = 1
+num2 = 2
+num3 = 3
 
-while num_int > 0:
-    num_int = int(input("Input a number: "))
+if n > 3: 
+    print(num1)
+    print(num2)
+    print(num3)
 
-    if num_int > max_int:
-        max_int=num_int
-    elif num_int < max_int:
-        max_int= max_int
-
-print("The maximum is", max_int)    # Do not change this line
+for x in range(3, n):
+    sum = num1 + num2 + num3
+    num1 = num2
+    num2 = num3
+    num3 = sum
+    print(sum)
